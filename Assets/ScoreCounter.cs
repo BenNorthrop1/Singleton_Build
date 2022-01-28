93c1442a1a6b452c9755ea6c6bd9f54a6707905e
BenNorthrop1 <90899118+BenNorthrop1@users.noreply.github.com>
2022-01-28 15:56:26 +0000

    Update

--- Assets/ScoreCounter.cs
+++ Assets/ScoreCounter.cs
@@ -7,44 +7,48 @@ using TMPro;
 public class ScoreCounter : MonoBehaviour
 {
 
-    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI currentScoreText;
 
+    public TextMeshProUGUI highScoreText;
+    
     int currentScore;
 
-    int highScore = 10000;
+    int highScore;
 
     
 
     // Start is called before the first frame update
     void Start()
     {
-        if( PlayerPrefs.HasKey("HighScore") == false )
+        if(PlayerPrefs.HasKey("HighScore") == true)
         {
-            PlayerPrefs.SetInt("HighScore", currentScore);
-            highScore = currentScore;
-        
+            highScore = PlayerPrefs.GetInt("HighScore");
         }
-        
-
-        
+        else
+        {
+            highScore = 10;
+        }
+        highScoreText.text = "Highest Score :" + Convert.ToString(highScore);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space"))
+
+        if(Input.GetKeyDown("b"))
         {
-            currentScore++;
-            scoreText.text = "Current score :" + Convert.ToString(currentScore);
+            currentScore++; 
         }
-
-
-
+        else
+        {
+            currentScoreText.text = "Current score :" + Convert.ToString(currentScore);
+        }
 
         if (currentScore >= highScore)
         {
-            currentScore = highScore;
             PlayerPrefs.SetInt("HighScore" , highScore);
+            highScore = currentScore;
+            highScoreText.text = "Highest score :" + Convert.ToString(highScore);
         }
     }
 }
